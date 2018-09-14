43047640ed84f0ee4a0f4a305ccdc9f2c4e995db
INABA Akira <inaba@cerasus.inabaakira.org>
2018-09-14 09:44:46 +0900

    [Chap.02] 2.2.10 Routes as Rack Endpoints

--- rails_5_2_1/config/routes.rb
+++ rails_5_2_1/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "/hello", to: proc { |env| [200, {}, ["Hello world"]] }
+  # mount HelloApp, at: '/hello'
+  mount HelloApp => '/hello'
 end
