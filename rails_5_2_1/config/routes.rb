0fd19b7b9f427babdb6818923c75a193fe0f1e95
INABA Akira <inaba@cerasus.inabaakira.org>
2018-10-11 16:48:53 +0900

    [Chap.02] 2.4.1 'help' route を作成。

--- rails_5_2_1/config/routes.rb
+++ rails_5_2_1/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get "/hello", to: proc { |env| [200, {}, ["Hello world"]] }
   # mount HelloApp, at: '/hello'
-  mount HelloApp => '/hello'
+  # mount HelloApp => '/hello'
+  get "/help" => 'help#index', as: 'help'
 end
