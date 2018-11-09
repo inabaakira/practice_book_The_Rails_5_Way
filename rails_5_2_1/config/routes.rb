fa9252c6a124a18025c2fc99827a1991a6b03962
INABA Akira <inaba@cerasus.inabaakira.org>
2018-11-09 11:06:43 +0900

    [Chap.02] 2.5.5 Bundling Constraints 全体にかける

--- rails_5_2_1/config/routes.rb
+++ rails_5_2_1/config/routes.rb
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
   # scope :auctions, :archived do
   #   get ':id' => "items#show"
   # end
-  scope :auctions, controller: :auctions, as: 'admin' do
-    get 'new' => :new, as: 'new_auction'
+  # scope :auctions, controller: :auctions, as: 'admin' do
+  #   get 'new' => :new, as: 'new_auction'
+  # end
+  scope controller: :auctions, constraints: {:id => /\d+/} do
+    get 'edit/:id' => :edit
+    post 'pause/:id' => :pause
   end
 end
