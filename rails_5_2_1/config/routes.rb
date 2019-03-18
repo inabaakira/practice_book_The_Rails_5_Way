f266f4e0c2b6a3be1204708fb49a37207e83dd9d
INABA Akira <ainaba@idcf.jp>
2019-03-18 18:01:37 +0900

    [Chap.03] 3.9.2 Extra Collection Routes: 終了

--- rails_5_2_1/config/routes.rb
+++ rails_5_2_1/config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   get "/help" => 'help#index', as: 'help'
 
   resources :auctions do
-    resources :bids do
-      match :retract, via: [:get, :post], on: :member
-    end
+    match :terminate, via: [:get, :post], on: :collection
   end
 end
