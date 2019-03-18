ae40cc009990eabe601aadef82b7bba2104a67fc
INABA Akira <ainaba@idcf.jp>
2019-03-18 16:19:24 +0900

    [Chap.03] 3.8 ROUTING CONCERNS: 終了

--- rails_5_2_1/config/routes.rb
+++ rails_5_2_1/config/routes.rb
@@ -1,46 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'root#index'
-  # get "/hello", to: proc { |env| [200, {}, ["Hello world"]] }
-  # mount HelloApp, at: '/hello'
-  # mount HelloApp => '/hello'
   get "/help" => 'help#index', as: 'help'
-  # get "/item/:id" => "items#show", as: "item"
-  # get "/auction/:auction_id/item/:id" => "items#show", as: "item"
-  # scope controller: :help do
-  #   get 'help/index' => :index
+
+  # resources :auctions do
+  #   resources :bids
+  #   resources :comments
+  #   resources :image_attachments, only: :index
   # end
-  # scope :auctions, :archived do
-  #   get ':id' => "items#show"
+
+  # resources :bids do
+  #   resources :comments
   # end
-  # scope :auctions, controller: :auctions, as: 'admin' do
-  #   get 'new' => :new, as: 'new_auction'
-  # end
-  # scope controller: :auctions, constraints: {:id => /\d+/} do
-  #   get 'edit/:id' => :edit
-  #   post 'pause/:id' => :pause
-  # end
-  # scope path: '/auctions', controller: :auctions do
-  #   get '/' => :index
-  #   get 'index' => :index
-  #   get 'new' => :new
-  #   get ':id' => "auctions#show", as: 'auction'
-  #   constraints id: /\d+/ do
-  #     get 'edit/:id' => :edit
-  #     post 'pause/:id' => :pause
-  #   end
-  #   constraints(DateFormatConstraint) do
-  #     get 'since/:date' => :since
-  #   end
-  # end
-  resources :auctions do
-    shallow do
-      resources :bids do
-        resources :comments
-      end
-    end
+
+  concern :commentable do
+    resources :comments
   end
 
-  direct(:apple) { "http://www.apple.com" }
-  resource :profile
+  concern :image_attachable do
+    resources :image_attachments, only: :index
+  end
+
+  resources :auctions, concerns: [:commentable, :image_attachable] do
+    resources :bids
+  end
+
+  resources :bids, concerns: :commentable
 end
